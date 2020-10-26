ab6c163cd200271f96b2fd3629a38ae8e12f5354
Osamu Ogasawara <osamu.ogasawara@gmail.com>
2020-10-26 20:31:31 +0900

    Add test

--- test/lib/DuplicatedKeyUniqueValueHashMap.spec.ts
+++ test/lib/DuplicatedKeyUniqueValueHashMap.spec.ts
@@ -56,4 +56,28 @@ describe('DuplicatedKeyUniqueValueHashMap', () => {
 
 
 
+    context("removeKey", () => {
+        it('should accept both DataCells of differet keys.', () => {
+            const hashmap = new DuplicatedKeyUniqueValueHashMap<string, string>();
+            hashmap.put("A001", "value1");
+            hashmap.put("A002", "value1");
+            hashmap.put("A001", "value2");
+            hashmap.put("A002", "value2");
+
+            let result: string[] = hashmap.getValues("A001");
+            expect(result[0]).to.equal("value1");
+            expect(result[1]).to.equal("value2");
+
+
+            hashmap.removeKey("A001");
+            expect(hashmap.containsKey("A001")).to.be.false;
+            expect(hashmap.containsKey("A002")).to.be.true;
+            expect(hashmap.keySet().size()).to.equal(1);
+        });
+
+    });
+
+
+
+
 });
