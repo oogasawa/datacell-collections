9d943569a50d51b88ddf46439e230d61b928b212
Osamu Ogasawara <osamu.ogasawara@gmail.com>
2020-11-12 06:35:53 +0900

    Refactor to use jest framework

--- test/lib/DuplicatedKeyUniqueValueHashMap.spec.ts
+++ test/lib/DuplicatedKeyUniqueValueHashMap.spec.ts
@@ -1,9 +1,6 @@
 
-import { expect } from 'chai';
-import 'mocha';
-
 import { DuplicatedKeyUniqueValueHashMap } from "../../src/lib/DuplicatedKeyUniqueValueHashMap";
-import { array_isSubset } from "../../src/lib/ArrayUtil";
+import * as arraylib from "datacell-arraylib";
 
 import * as log4js from "log4js";
 const logger = log4js.getLogger();
@@ -11,25 +8,24 @@ const logger = log4js.getLogger();
 
 
 
-
 describe('DuplicatedKeyUniqueValueHashMap', () => {
 
-    context("constructor", () => {
+    describe("constructor", () => {
         it('should create an empty hashmap.', () => {
             const hashmap = new DuplicatedKeyUniqueValueHashMap<string, string>();
-            expect(hashmap.isEmpty()).to.equal(true);
-            expect(hashmap.size()).to.equal(0);
+            expect(hashmap.isEmpty()).toEqual(true);
+            expect(hashmap.size()).toEqual(0);
         });
 
     });
 
 
-    context("put", () => {
+    describe("put", () => {
         it('should accept both DataCells of differet keys.', () => {
             const hashmap = new DuplicatedKeyUniqueValueHashMap<string, string>();
             hashmap.put("A001", "value1");
             hashmap.put("A002", "value1");
-            expect(hashmap.size()).to.equal(2);
+            expect(hashmap.size()).toEqual(2);
         });
 
 
@@ -47,26 +43,26 @@ describe('DuplicatedKeyUniqueValueHashMap', () => {
                 })
             });
             logger.level = "error";
-            expect(hashmap.size()).to.equal(2);
+            expect(hashmap.size()).toEqual(2);
         });
 
         it('should reject DataCells with the same key / value.', () => {
             const hashmap = new DuplicatedKeyUniqueValueHashMap<string, string>();
             hashmap.put("A001", "value1");
-            expect(hashmap.size()).to.equal(1);
+            expect(hashmap.size()).toEqual(1);
             hashmap.put("A001", "value2");
-            expect(hashmap.size()).to.equal(2);
+            expect(hashmap.size()).toEqual(2);
             hashmap.put("A001", "value1");
-            expect(hashmap.size()).to.equal(2);
+            expect(hashmap.size()).toEqual(2);
             hashmap.put("A001", "value2");
-            expect(hashmap.size()).to.equal(2);
+            expect(hashmap.size()).toEqual(2);
         });
 
     });
 
 
 
-    context("clear", () => {
+    describe("clear", () => {
         it('should remove all key-values from the hashmap.', () => {
             const hashmap = new DuplicatedKeyUniqueValueHashMap<string, string>();
             hashmap.put("A001", "value1");
@@ -74,23 +70,23 @@ describe('DuplicatedKeyUniqueValueHashMap', () => {
             hashmap.put("A001", "value2");
             hashmap.put("A002", "value2");
 
-            expect(hashmap.size()).to.equal(4);
+            expect(hashmap.size()).toEqual(4);
 
             hashmap.clear();
-            expect(hashmap.size()).to.equal(0);
+            expect(hashmap.size()).toEqual(0);
 
             hashmap.put("A001", "value1");
             hashmap.put("A002", "value1");
             hashmap.put("A001", "value2");
 
-            expect(hashmap.size()).to.equal(3);
+            expect(hashmap.size()).toEqual(3);
 
         });
 
     });
 
 
-    context("containsKey", () => {
+    describe("containsKey", () => {
         it('should return if given key is contained in the hashmap.', () => {
             const hashmap = new DuplicatedKeyUniqueValueHashMap<string, string>();
             hashmap.put("A001", "value1");
@@ -98,15 +94,15 @@ describe('DuplicatedKeyUniqueValueHashMap', () => {
             hashmap.put("A001", "value2");
             hashmap.put("A002", "value2");
 
-            expect(hashmap.containsKey("A001")).to.be.true;
-            expect(hashmap.containsKey("A002")).to.be.true;
-            expect(hashmap.containsKey("A003")).to.be.false;
+            expect(hashmap.containsKey("A001")).toBeTruthy;
+            expect(hashmap.containsKey("A002")).toBeTruthy;
+            expect(hashmap.containsKey("A003")).toBeFalsy;
 
         });
 
     });
 
-    context("getValues", () => {
+    describe("getValues", () => {
         it('should return values corresponding to the given key as a string array.', () => {
             const hashmap = new DuplicatedKeyUniqueValueHashMap<string, string>();
             hashmap.put("A001", "value1");
@@ -119,15 +115,15 @@ describe('DuplicatedKeyUniqueValueHashMap', () => {
             // logger.debug(ans);
             // logger.level = "error";
 
-            expect(array_isSubset(ans, ["value1", "value2"])).to.be.true;
-            expect(array_isSubset(["value1", "value2"], ans)).to.be.true;
+            expect(arraylib.isSubset(ans, ["value1", "value2"])).toBeTruthy;
+            expect(arraylib.isSubset(["value1", "value2"], ans)).toBeTruthy;
 
 
         });
 
     });
 
-    context("isEmpty", () => {
+    describe("isEmpty", () => {
         it('should return if the hash map is empty.', () => {
             const hashmap = new DuplicatedKeyUniqueValueHashMap<string, string>();
             hashmap.put("A001", "value1");
@@ -135,19 +131,19 @@ describe('DuplicatedKeyUniqueValueHashMap', () => {
             hashmap.put("A001", "value2");
             hashmap.put("A002", "value2");
 
-            expect(hashmap.size()).to.equal(4);
-            expect(hashmap.isEmpty()).to.be.false;
+            expect(hashmap.size()).toEqual(4);
+            expect(hashmap.isEmpty()).toBeFalsy;
 
             hashmap.clear();
-            expect(hashmap.size()).to.equal(0);
-            expect(hashmap.isEmpty()).to.be.true;
+            expect(hashmap.size()).toEqual(0);
+            expect(hashmap.isEmpty()).toBeTruthy;
         });
 
     });
 
 
 
-    context("keySet", () => {
+    describe("keySet", () => {
         it('should return a Set object of keys.', () => {
             const hashmap = new DuplicatedKeyUniqueValueHashMap<string, string>();
             hashmap.put("A001", "value1");
@@ -156,22 +152,22 @@ describe('DuplicatedKeyUniqueValueHashMap', () => {
             hashmap.put("A002", "value2");
 
             const set: Set<string> = hashmap.keySet();
-            expect(set.has("A001")).to.be.true;
-            expect(set.has("A002")).to.be.true;
+            expect(set.has("A001")).toBeTruthy;
+            expect(set.has("A002")).toBeTruthy;
 
             // logger.level = "debug";
             // logger.debug(Array.from(set));
             // logger.level = "error";
 
-            expect(array_isSubset(Array.from(set), ["A001", "A002"])).to.be.true;
-            expect(array_isSubset(["A001", "A002"], Array.from(set))).to.be.true;
+            expect(arraylib.isSubset(Array.from(set), ["A001", "A002"])).toBeTruthy;
+            expect(arraylib.isSubset(["A001", "A002"], Array.from(set))).toBeTruthy;
 
         });
 
     });
 
 
-    context("removeKey", () => {
+    describe("removeKey", () => {
         it('should removes a key and corresponding values', () => {
             const hashmap = new DuplicatedKeyUniqueValueHashMap<string, string>();
             hashmap.put("A001", "value1");
@@ -179,31 +175,31 @@ describe('DuplicatedKeyUniqueValueHashMap', () => {
             hashmap.put("A001", "value2");
             hashmap.put("A002", "value2");
 
-            expect(hashmap.size()).to.equal(4);
+            expect(hashmap.size()).toEqual(4);
 
             hashmap.removeKey("A001");
-            expect(hashmap.size()).to.equal(2);
+            expect(hashmap.size()).toEqual(2);
 
             hashmap.removeKey("A002");
-            expect(hashmap.size()).to.equal(0);
-            expect(hashmap.isEmpty()).to.be.true;
+            expect(hashmap.size()).toEqual(0);
+            expect(hashmap.isEmpty()).toBeTruthy;
         });
 
         it('should ignore nonexistent keys.', () => {
             const hashmap = new DuplicatedKeyUniqueValueHashMap<string, string>();
             hashmap.removeKey("A000");
-            expect(hashmap.size()).to.equal(0);
-            expect(hashmap.isEmpty()).to.be.true;
+            expect(hashmap.size()).toEqual(0);
+            expect(hashmap.isEmpty()).toBeTruthy;
 
             hashmap.put("A001", "value1");
             hashmap.put("A002", "value1");
             hashmap.put("A001", "value2");
             hashmap.put("A002", "value2");
 
-            expect(hashmap.size()).to.equal(4);
+            expect(hashmap.size()).toEqual(4);
 
             hashmap.removeKey("A000");
-            expect(hashmap.size()).to.equal(4);
+            expect(hashmap.size()).toEqual(4);
         });
 
 
